9491bf4537a8c65fb7de12b56951a66fb80bf78d
kebman <kebman@journalist.com>
2015-09-12 00:24:37 +0200

    Update dupefinder_init.sql

--- dupefinder_init.sql
+++ dupefinder_init.sql
@@ -1,14 +1,16 @@
-CREATE TABLE if not exists file_info (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	filename TEXT,
+CREATE TABLE if not exists "files" (
 	md5 TEXT,
-	size INTEGER,
-	mtime TEXT,
-	btime TEXT
+	filename TEXT,
+	mtime INTEGER,
+	btime INTEGER,
+	size INTEGER
 );
+
+-- note: no ID field needed as md5 hash is sufficient for indexing and easy comparison
 
 -- note: SQLite3 has no date type, though it has functions to handle date strings
 -- in this project, dates will be handled by Node.js
+-- however js dates automatically get stored as unix timestamps
 
 -- some system vars not stored, like inode, because I see no apparent use in it pertainint to this program
 -- atime i.e. last accessed not needed for project
