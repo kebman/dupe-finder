99b1979ee7185fd173cb9c8faefccd9862b21c57
kebman <kebman@journalist.com>
2015-10-02 05:15:05 +0200

    Update nondb_app.js
    First working update, but needs ARGV support

--- nondb_app.js
+++ nondb_app.js
@@ -1,10 +1,9 @@
 // generate and compare a list of files with stat and hash… without using sqlite3
 // …while using file streaming with promises
-// milestone 1 reached: read dir A and output fname, stat and hashes using promises
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
 var crypto = require('crypto');
-
+var path = require('path');
 // terminal or console argument handling
 /*
 if (process.argv.length < 3) {
@@ -21,38 +20,78 @@ if (process.argv.length = 4) {
 */
 // for now, though, we'll just leave it for testing
 
-var readdirPromise = fs.readdirAsync(".").map(function(fileName) {
-	var statPromise = fs.statAsync(fileName);
-	
-	return Promise.join(statPromise, fileName, function(statPromise, fileName) {
+// find duplicate files with Node.js, cuz, you know, it's useful
+
+var pathA = "."; // folder you're in, wherever that might be
+var pathB = "/path/to/the/directory/you/want/to/compare/it/to"; // yes, yes, argv, etc, but I haven't bothered yet, ok!
+
+
+var hashes = []; // where the magical hashes will be stored - but wait, that's not all, now also with file info!
+
+// I dub this: promise hell! (or possibly this could just be done eons better, but well, it works...)
+function hashDirIn(folder) {
+	var pathPromiseA = fs.readdirAsync(folder).map(function(fileName) {
+		var workPath = path.join(folder, fileName);
+		var statPromise = fs.statAsync(workPath);
 		
-		if(statPromise.isFile()) {
+		return Promise.join(statPromise, fileName, function(statPromise, fileName) {
+			
+			if(statPromise.isFile()) {
 
-			function makeStream(file, callback) {
-				var result = fs.createReadStream(file);
-				return callback(result);
-			}
+				function makeStream(file, callback) {
+					var result = fs.createReadStream(workPath);
+					return callback(result);
+				}
 
-			function process(stream) {
-				var hash = crypto.createHash('md5'); 
-				var dig = "";
-				return new Promise(function(resolve, reject) {
-					stream.on('data', function updateProcess(chunk) {
-						hash.update(chunk, 'utf8');
+				function process(stream) {
+					var hash = crypto.createHash('md5'); 
+					return new Promise(function(resolve, reject) {
+						stream.on('data', function updateProcess(chunk) {
+							hash.update(chunk, 'utf8');
+						});
+						stream.on('end', resolve);
+					}).then(function publish() {
+						var digest = hash.digest('hex');
+						hashes.push({digest:digest, path:workPath});
 					});
-					stream.on('end', resolve);
-				}).then(function publish() {
-					var digest = hash.digest('hex');
-					dig = digest;
-					console.log(digest + ": "+ fileName+ " - "+statPromise.size+ " bytes");
-					// return digest;
-				});
-			}
+				}
 
-			makeStream(fileName, process);
+				return makeStream(fileName, process);
 
-		} // endif
+			} // endif :p
 
+		});
+
+	}).then(function(){
+		// sort and display dupes
+		if(i==1) {
+			hashes.sort(function(a, b) {
+				if (a.digest < b.digest) {
+					return -1;
+				}
+				if (a.digest > b.digest) {
+					return 1;
+				}
+				return 0;
+			});
+			var dupe = 1;
+			hashes.map(function(obj, index) {
+				// note: currently this won't deal gracefully with more than two equal files at a time, though it will find them all (gotta find them all!)
+				if (index-1 >= 0) {
+					if(obj.digest == hashes[index-1].digest) {
+						console.log("Dupe "+dupe+" found:");
+						console.log(obj.path);
+						console.log("Equal to:")
+						console.log(hashes[index-1].path+"\n");
+						dupe++;
+					}
+				}
+			});
+		}
+		i++; // why do I feel this is cheating?
 	});
-	
-});
+}
+var i = 0;
+hashDirIn(pathA);
+hashDirIn(pathB);
+// but how to check both concurrently instead of one after the other? Ah well, it's late and my bottle of Ileach is soon empty...
